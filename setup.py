51d31a86fc20a161f544f981c25c132d8ba3d4a4
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:19:23 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_nicholsdiversity',
-    py_modules=['cldfbench_nicholsdiversity'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'nicholsdiversity=cldfbench_nicholsdiversity:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
